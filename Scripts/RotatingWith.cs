836650f135a2213436f995df8c9ae5f0ee089344
Maxwell Weil <maxwellsweil87@gmail.com>
2020-06-12 11:27:20 -0700

    Added scripts

---
+++ Scripts/RotatingWith.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class RotatingWith : MonoBehaviour
+{
+    public Transform rotObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = new Vector3(this.transform.localEulerAngles.x, rotObj.transform.localEulerAngles.y, this.transform.localEulerAngles.z);
+
+        this.transform.localEulerAngles = pos;
+    }
+}
